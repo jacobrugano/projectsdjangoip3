0885c2d7fb9852d4b8742243ea905674a5c3e5a8
github_username <jacobrugano@gmail.com>
2022-04-12 13:41:58 +0300

    Added code to enable the ADD button to work

--- gitprojects/views.py
+++ gitprojects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Category, Project
 
 # Create your views here.
@@ -15,4 +15,28 @@ def viewProject(request, pk):
 
 def addProject(request):
     categories = Category.objects.all()
+
+    # To capture the data uploaded by the form:
+    # First verify that there is actual data submitted thro:
+    if request.method == 'POST': #To check the request type is POST
+        data = request.POST #To get the form-fields data
+        project = request.FILES.get('project') #To get the uploaded project, project  been the name of that upload button in html
+   
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(
+                name=data['category_new'])
+        else:
+            category = None
+        
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+
+        return redirect('gallery')
+
+
     return render(request, 'gitprojects/add.html', {'categories':categories})
